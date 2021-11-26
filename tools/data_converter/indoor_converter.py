4d312daec3c7a8cdcf043e5f64ffaabecff0a4e8
Zhimin Sun <zhimin.sun@sjtu.edu.cn>
2021-11-26 16:09:17 +0800

    [Fix] Add converter for alfred dataset

--- tools/data_converter/indoor_converter.py
+++ tools/data_converter/indoor_converter.py
@@ -3,6 +3,7 @@ import mmcv
 import numpy as np
 import os
 
+from tools.data_converter.alfred_data_utils import AlfredData
 from tools.data_converter.s3dis_data_utils import S3DISData, S3DISSegData
 from tools.data_converter.scannet_data_utils import ScanNetData, ScanNetSegData
 from tools.data_converter.sunrgbd_data_utils import SUNRGBDData
@@ -25,13 +26,13 @@ def create_indoor_info_file(data_path,
         workers (int): Number of threads to be used. Default: 4.
     """
     assert os.path.exists(data_path)
-    assert pkl_prefix in ['sunrgbd', 'scannet', 's3dis'], \
+    assert pkl_prefix in ['sunrgbd', 'scannet', 's3dis', 'alfred'], \
         f'unsupported indoor dataset {pkl_prefix}'
     save_path = data_path if save_path is None else save_path
     assert os.path.exists(save_path)
 
     # generate infos for both detection and segmentation task
-    if pkl_prefix in ['sunrgbd', 'scannet']:
+    if pkl_prefix in ['sunrgbd', 'scannet', 'alfred']:
         train_filename = os.path.join(save_path,
                                       f'{pkl_prefix}_infos_train.pkl')
         val_filename = os.path.join(save_path, f'{pkl_prefix}_infos_val.pkl')
@@ -41,11 +42,18 @@ def create_indoor_info_file(data_path,
                 root_path=data_path, split='train', use_v1=use_v1)
             val_dataset = SUNRGBDData(
                 root_path=data_path, split='val', use_v1=use_v1)
-        else:
+        elif pkl_prefix == 'scannet':
             # ScanNet has a train-val-test split
             train_dataset = ScanNetData(root_path=data_path, split='train')
             val_dataset = ScanNetData(root_path=data_path, split='val')
             test_dataset = ScanNetData(root_path=data_path, split='test')
+            test_filename = os.path.join(save_path,
+                                         f'{pkl_prefix}_infos_test.pkl')
+        else:
+            # Alfred has a train-val-test split
+            train_dataset = AlfredData(root_path=data_path, split='train')
+            val_dataset = AlfredData(root_path=data_path, split='val')
+            test_dataset = AlfredData(root_path=data_path, split='test')
             test_filename = os.path.join(save_path,
                                          f'{pkl_prefix}_infos_test.pkl')
 
@@ -58,7 +66,7 @@ def create_indoor_info_file(data_path,
         mmcv.dump(infos_val, val_filename, 'pkl')
         print(f'{pkl_prefix} info val file is saved to {val_filename}')
 
-    if pkl_prefix == 'scannet':
+    if pkl_prefix in ['scannet', 'alfred']:
         infos_test = test_dataset.get_infos(
             num_workers=workers, has_label=False)
         mmcv.dump(infos_test, test_filename, 'pkl')
